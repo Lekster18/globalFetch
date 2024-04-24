4349fd75c2395f8cd223c9d5cb8f3e89ef6dcabd
Leks <lekee96@gmail.com>
2024-04-25 01:12:12 +0800

    update db and port

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const pool = require("./src/db/db");
+
+const trip = require("./src/routers/trip");
+
+const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(5001, () => {
-  console.log("server has started on port 5001");
+app.use("/api", trip);
+
+app.listen(5010, () => {
+  console.log("server has started on port 5010");
 });
+
+module.exports = app;
