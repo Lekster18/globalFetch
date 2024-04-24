d969291981770d636629cd11680774b23f7dd215
Leks <lekee96@gmail.com>
2024-04-25 01:46:57 +0800

    add fetchData

---
+++ globalFetch/src/hooks/useFetch.tsx
@@ -0,0 +1,45 @@
+const useFetch = () => {
+  const fetchData = async (
+    endpoint: string,
+    method: string,
+    body: any,
+    token?: string
+  ) => {
+    const res = await fetch("http://localhost:5010" + endpoint, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await res.json();
+
+    let returnValue: { ok: boolean; data: any } = {
+      ok: false,
+      data: "An error has occurred",
+    };
+    if (res.ok) {
+      if (data.status === "error") {
+        returnValue = { ok: false, data: data.message };
+      } else {
+        returnValue = { ok: true, data };
+      }
+    } else {
+      if (data?.errors && Array.isArray(data.errors)) {
+        const messages = data.errors.map((item: any) => item.msg);
+        returnValue = { ok: false, data: messages };
+      } else if (data?.status === "error") {
+        returnValue = { ok: false, data: data.message || data.msg };
+      } else {
+        console.log(data);
+        returnValue = { ok: false, data: "An error has occurred" };
+      }
+    }
+
+    return returnValue;
+  };
+
+  return fetchData;
+};
+
+export default useFetch;
