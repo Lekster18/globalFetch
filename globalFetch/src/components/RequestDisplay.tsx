26157cf1a51068859bd6ab15ca4b56ab521af0aa
Leks <lekee96@gmail.com>
2024-04-26 00:36:37 +0800

    add request components

---
+++ globalFetch/src/components/RequestDisplay.tsx
@@ -0,0 +1,136 @@
+import React, { useEffect, useRef, useState } from "react";
+import Request from "./Request";
+import useFetch from "../hooks/useFetch";
+
+interface RequestData {
+  id: number;
+  date: string;
+  price: number;
+  description: string;
+  country: string;
+  city: string;
+  deleteRequest: (id: number) => void;
+}
+
+const RequestDisplay: React.FC = () => {
+  const [request, setRequest] = useState<RequestData[]>([]);
+  const fetchData = useFetch();
+
+  const countryRef = useRef<HTMLInputElement>(null);
+  const cityRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLInputElement>(null);
+  const priceRef = useRef<HTMLInputElement>(null);
+  const dateRef = useRef<HTMLInputElement>(null);
+
+  const getRequest = async () => {
+    const res = await fetchData("/api/request", "GET", undefined);
+
+    if (res.ok) {
+      setRequest(res.data);
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
+
+  const addRequest = async () => {
+    const res = await fetchData("/api/request", "POST", {
+      description: descriptionRef.current!.value,
+      price: priceRef.current!.value,
+      date: dateRef.current!.value,
+      country: countryRef.current!.value,
+      city: cityRef.current!.value,
+    });
+
+    if (res.ok) {
+      getRequest;
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
+
+  const deleteRequest = async (id: number) => {
+    const res = await fetchData("/api/request/" + id, "DELETE", undefined);
+    if (res.ok) {
+      getRequest();
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
+
+  useEffect(() => {
+    getRequest();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="col-md-6">Requests</h1>
+      </div>
+
+      <div className="row">
+        <input
+          type="text"
+          ref={descriptionRef}
+          placeholder="Enter Item Description"
+          className="col-md-1"
+        />
+        <input
+          type="text"
+          ref={priceRef}
+          placeholder="Price"
+          className="col-md-1"
+        />
+        <input
+          type="text"
+          ref={countryRef}
+          placeholder="Country"
+          className="col-md-1"
+        />
+        <input
+          type="text"
+          ref={cityRef}
+          placeholder="City"
+          className="col-md-1"
+        />
+        <input
+          type="text"
+          ref={dateRef}
+          placeholder="Date of collection"
+          className="col-md-1"
+        />
+        <button className="col-md-1" onClick={addRequest}>
+          add
+        </button>
+      </div>
+
+      <br />
+      <br />
+
+      <div className="row">
+        <div className="col-md-1">Item Description</div>
+        <div className="col-md-1">Price</div>
+        <div className="col-md-1">Country</div>
+        <div className="col-md-1">City</div>
+        <div className="col-md-1">Collection date</div>
+      </div>
+
+      {request.map((item) => (
+        <Request
+          id={item.id}
+          country={item.country}
+          city={item.city}
+          date={item.date}
+          price={item.price}
+          description={item.description}
+          getRequest={getRequest}
+          deleteRequest={deleteRequest}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RequestDisplay;
---
+++ globalFetch/src/components/RequestUpdateModal.tsx
@@ -0,0 +1,142 @@
+import { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+// import styles from "./Modal.module.css";
+import useFetch from "../hooks/useFetch";
+
+interface RequestOverLayProps {
+  id: number;
+  date: string;
+  price: number;
+  description: string;
+  country: string;
+  city: string;
+  setShowReqUpdateModal: (show: boolean) => void;
+  getRequest: () => void;
+}
+
+const RequestOverLay: React.FC<RequestOverLayProps> = (props) => {
+  const fetchData = useFetch();
+  const countryRef = useRef<HTMLInputElement>(null);
+  const cityRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLInputElement>(null);
+  const priceRef = useRef<HTMLInputElement>(null);
+  const dateRef = useRef<HTMLInputElement>(null);
+
+  const updateRequest = async (id: number) => {
+    const res = await fetchData(`/api/request/${id}`, "PUT", {
+      country: countryRef.current!.value,
+      city: cityRef.current!.value,
+      description: descriptionRef.current!.value,
+      price: priceRef.current!.value,
+      date: dateRef.current!.value,
+    });
+
+    if (res.ok) {
+      props.getRequest();
+      props.setShowReqUpdateModal(false);
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      countryRef.current &&
+      cityRef.current &&
+      dateRef.current &&
+      priceRef.current &&
+      descriptionRef.current
+    ) {
+      countryRef.current.value = props.country;
+      cityRef.current.value = props.city;
+      dateRef.current.value = props.date;
+      priceRef.current.value = props.price.toString();
+      descriptionRef.current.value = props.description;
+    }
+  }, [props.country, props.city, props.date, props.price, props.description]);
+  return (
+    <div>
+      <div>
+        <br />
+        <br />
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-1">Item description</div>
+          <input ref={descriptionRef} type="text" className="col-md-3" />
+          <div className="col-md-1"></div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-1">Price</div>
+          <input ref={priceRef} type="text" className="col-md-3" />
+          <div className="col-md-3"></div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-1">Collection Date</div>
+          <input ref={dateRef} type="text" className="col-md-3" />
+          <div className="col-md-3"></div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-1">Country</div>
+          <input ref={countryRef} type="text" className="col-md-3" />
+          <div className="col-md-3"></div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-1">City</div>
+          <input ref={cityRef} type="text" className="col-md-3" />
+          <div className="col-md-3"></div>
+        </div>
+
+        <br />
+        <div className="row">
+          <div className="col-md-3"></div>
+          <button onClick={() => updateRequest(props.id)} className="col-md-3">
+            update
+          </button>
+          <button
+            onClick={() => props.setShowReqUpdateModal(false)}
+            className="col-md-3"
+          >
+            cancel
+          </button>
+          <div className="col-md-3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface RequestUpdateModalProps extends RequestOverLayProps {
+  setShowReqUpdateModal: (show: boolean) => void;
+  getRequest: () => void;
+}
+
+const RequestUpdateModal: React.FC<RequestUpdateModalProps> = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <RequestOverLay
+          id={props.id}
+          country={props.country}
+          city={props.city}
+          date={props.date}
+          price={props.price}
+          description={props.description}
+          setShowReqUpdateModal={props.setShowReqUpdateModal}
+          getRequest={props.getRequest}
+        />,
+        document.querySelector("#modal-root")!
+      )}
+    </>
+  );
+};
+
+export default RequestUpdateModal;
